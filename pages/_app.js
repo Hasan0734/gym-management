8c988964d8b44555c2fb984aa6ac94cc8c500662
Jahid Hasan <jahidhasan2856@gmail.com>
2022-04-15 00:53:39 +0600

    layout complete

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,13 @@
-import '../styles/globals.css'
+
+import '../styles/css/bootstrap-extended.css';
+import '../styles/css/bootstrap.css';
+import '../styles/css/colors.css';
+import '../styles/css/components.css';
+import '../styles/css/core/menu/menu-types/vertical-menu.css';
+import '../styles/css/pages/authentication.css';
+
+// import '../styles/globals.css';
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
